8868c989d8fac0178f90f73dc294eb4bf4994e64
DatNoLove <datnolove85@gmail.com>
2025-11-27 08:57:35 +0700

    java5-test

---
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("title", "FPT Polytechnic");
+        model.addAttribute("subject", "Spring Boot MVC");
+        return "hello";
+    }
+
+}
